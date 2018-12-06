ddce97343823879f4e5fc9913aeb2a9d30e99b10
Cheeto836 <mprussell836@gmail.com>
2018-12-06 01:02:20 -0500

    began work on cmd options.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,30 @@
 #include <qapplication.h>
+#include <qcommandlineparser.h>
 
 #include "MainWindow.h"
+
+bool parseCmd(QCommandLineParser& parser);
 
 int main(int argc, char** argv)
 {
   QApplication app(argc, argv);
 
-  MainWindow mainWindow;
+  QCommandLineParser parser;
+  if (!parseCmd(parser))
+  {
+    parser.showHelp(1);
+  } //end  if (!parseCmd())
 
+  MainWindow mainWindow;
+  
   mainWindow.show();
 
   return app.exec();
 }
+
+bool parseCmd(QCommandLineParser& parser)
+{
+  //TODO implementation
+  return false;
+}
+
